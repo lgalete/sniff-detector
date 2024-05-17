5c6e1dc5ef60c3c2917246e413665b0986b1bcf2
Julio Zambom <julio.zambom@curseduca.com>
2024-05-17 10:16:14 -0300

    fix: handling with status when browser is in portuguese

--- src/modules/status.ts
+++ src/modules/status.ts
@@ -1,25 +1,38 @@
-import { Page } from 'puppeteer';
-import { UserStatus } from '@/types';
+import { Page } from "puppeteer";
+import { UserStatus, UserStatusMapper } from "@/types";
 
-export async function checkUserStatus(page: Page, userId: string): Promise<UserStatus | null> {
-    const userStatus = await page.evaluate((userId, UserStatus) => {
-        const userEl = document.querySelector(`.p-channel_sidebar__static_list__item[data-item-key="${userId}"]`);
-        const presenceIndicator = userEl.querySelector('*[data-qa="presence_indicator"]');
+export async function checkUserStatus(
+  page: Page,
+  userId: string
+): Promise<UserStatus | null> {
+  const userStatus = await page.evaluate(
+    (userId, UserStatus, UserStatusMapper) => {
+      const userEl = document.querySelector(`#${userId}`);
 
-        const userStatus = presenceIndicator?.getAttribute('title')?.toUpperCase() as string;
+      const presenceIndicator = userEl.querySelector(
+        '*[data-qa="presence_indicator"]'
+      );
 
-        if (!userStatus) {
-            return null;
-        }
+      const userStatus = presenceIndicator
+        ?.getAttribute("title")
+        ?.toUpperCase() as string;
 
-        if (userStatus.includes(UserStatus.Active)) {
-            return UserStatus.Active
-        }
+      if (!userStatus) {
+        return null;
+      }
 
-        if (userStatus.includes(UserStatus.Away)) {
-            return UserStatus.Away;
-        }
-    }, userId, UserStatus);
+      if (UserStatusMapper.Active.includes(userStatus)) {
+        return UserStatus.Active;
+      }
 
-    return userStatus;
+      if (UserStatusMapper.Away.includes(userStatus)) {
+        return UserStatus.Away;
+      }
+    },
+    userId,
+    UserStatus,
+    UserStatusMapper
+  );
+
+  return userStatus;
 }
